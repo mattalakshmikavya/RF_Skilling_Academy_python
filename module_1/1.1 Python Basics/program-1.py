f6bc372e474ce81e556e099a34a99cdca5128d69
mattalakshmikavya <mattalakshmikavya@gmail.com>
2026-05-22 20:45:41 +0530

    updating the readme.md and pushing the module 1

---
+++ module_1/1.1 Python Basics/program-1.py
@@ -0,0 +1,4 @@
+## 1. Write a simple Python script to print your name to the console.
+
+
+print("my name is Kavya")
---
+++ module_1/1.1 Python Basics/program-2.py
@@ -0,0 +1,7 @@
+## 2. Write a program that takes user input (name, age, etc.)
+#  and prints a personalized message.
+
+name=input("enter your name")
+age=input("enter your age")
+city=input("enter your city")
+print("Welcome to my gitbub account")

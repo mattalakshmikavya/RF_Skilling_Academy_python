760b52b1b6fbd75a64ea158d25f0e74a81d1396e
mattalakshmikavya <mattalakshmikavya@gmail.com>
2026-05-24 16:00:10 +0530

    Refined the second program

--- module_1/1.1 Python Basics/program-2.py
+++ module_1/1.1 Python Basics/program-2.py
@@ -1,8 +1,8 @@
 ## 2. Write a program that takes user input (name, age, etc.)
 #  and prints a personalized message.
 
-name=input("Enter your name")
-age=input("Enter your age")
-city=input("Enter your city")
+name=input("Enter your name:")
+age=input("Enter your age:")
+city=input("Enter your city:")
 print("Hello", name, "you are", age, "years old and you live in", city)
-print("Welcome to my github account")
+print("Welcome to Git hub account")
